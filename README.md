2a8fd914481d80c6d64c88a18e0005f194477c39
Vlad <vladklivak@gmail.com>
2019-02-16 14:30:49 +0200

    Added snake and food@

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 | # | Game        | Source                                      | Demo  |
 | - | ----------- | :-----------------------------------------: | ----- |
-| 1 | Snake       | [YouTube](https://www.youtube.com/playlist?list=PLW5DjTjujN9y21AOeJ3nfv5X5V45wQNEO)                        | link  |
+| 1 | Snake       | [YouTube](https://www.youtube.com/playlist?list=PLW5DjTjujN9y21AOeJ3nfv5X5V45wQNEO)                        | ---  |
--- src/views/GameSnake.vue
+++ src/views/GameSnake.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="game-snake">
-    <h2>Game snake</h2>
     <canvas ref="gameScreen"></canvas>
   </div>
 </template>
@@ -10,22 +9,97 @@ export default {
   data() {
     return {
       snake: null,
+      snakeLength: null,
+      snakeSize: null,
+
+      food: null,
+
+      canvas: null,
       context: null,
       screenWidth: null,
       screenHeight: null
     };
   },
   mounted() {
-    this.getInitialize();
-    console.log(this.snake);
+    this.gameInitialize();
+    this.snakeInitialize();
+    this.foodInitialize();
+
+    setInterval(this.gameLoop, 1000 / 30);
   },
   methods: {
-    getInitialize() {
-      this.snake = this.$refs.gameScreen;
-      this.context = this.snake.getContext("2d");
+    // scene methods
+    gameInitialize() {
+      this.canvas = this.$refs.gameScreen;
+      this.context = this.canvas.getContext("2d");
+
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+
+      this.canvas.width = this.screenWidth;
+      this.canvas.height = this.screenHeight;
     },
-    gameLoop() {},
-    gameDraw() {}
+
+    gameLoop() {
+      this.gameDraw();
+      this.snakeUpdate();
+      this.snakeDraw();
+      this.foodDraw();
+    },
+
+    gameDraw() {
+      this.context.fillStyle = "rgb(180, 250, 213)";
+      this.context.fillRect(0, 0, this.screenWidth, this.screenHeight);
+    },
+
+    // snake methods
+    snakeInitialize() {
+      this.snake = [];
+      this.snakeLength = 15;
+      this.snakeSize = 20;
+
+      for (let index = this.snakeLength - 1; index >= 0; index--) {
+        this.snake.push({
+          x: index,
+          y: 0
+        });
+      }
+    },
+    snakeDraw() {
+      for (let index = 0; index < this.snake.length; index++) {
+        this.context.fillStyle = "white";
+        this.context.fillRect(
+          this.snake[index].x * this.snakeSize,
+          this.snake[index].y * this.snakeSize,
+          this.snakeSize,
+          this.snakeSize
+        );
+      }
+    },
+    snakeUpdate() {
+      let snakeHeadX = this.snake[0].x;
+      let snakeHeadY = this.snake[0].y;
+
+      snakeHeadX++;
+
+      let snakeTail = this.snake.pop();
+      snakeTail.x = snakeHeadX;
+      snakeTail.y = snakeHeadY;
+
+      this.snake.unshift(snakeTail);
+    },
+
+    // food
+    foodInitialize() {
+        this.food = {
+            x: 0,
+            y: 0
+        };
+    },
+    foodDraw() {
+        this.context.fillStyle = "white";
+        this.context.fillRect(this.food.x, this.food.y, this.snakeSize, this.snakeSize);
+    }
   }
 };
 </script>
